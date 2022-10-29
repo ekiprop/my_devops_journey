31596139a5df51f49e6e4a81c32309dd44dd6dbb
ekiprop <evanskiprop1@gmail.com>
2022-10-29 22:08:15 +0300

    kk

--- go/greetings/greetings.go
+++ go/greetings/greetings.go
@@ -1,9 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+)
 
-func Hello(name string) string {
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+    // If no name was given, return an error with a message.
+    if name == "" {
+        return "", errors.New("empty name")
+    }
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+    // If a name was received, return a value that embeds the name
+    // in a greeting message.
+    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    return message, nil
 }
